9b8a72f9a3444cf108619803bc8292a609df9493
Manuel-Alarcon360 <manuelfernandoalarconr@gmail.com>
2026-01-19 15:09:27 -0500

    cambio de dominio de peticion de clientes a docalendar}

--- historia_clinica/settings.py
+++ historia_clinica/settings.py
@@ -27,7 +27,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)   #manuel
     
 #ALLOWED_HOSTS = ['*']   #manuel
 
-URL_CLIENTES_EXTERNOS = config('URL_CLIENTES_EXTERNOS', default='http://127.0.0.1:8000/api/')
+URL_CLIENTES_EXTERNOS = config('URL_CLIENTES_EXTERNOS', default='https://docalendar.com.co/api/')
 
 # Application definition
 
