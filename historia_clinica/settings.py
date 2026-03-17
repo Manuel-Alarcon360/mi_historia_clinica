a15a5141b3216bd40701c39eb536883af16c056e
Manuel-Alarcon360 <manuelfernandoalarconr@gmail.com>
2026-03-17 16:27:45 -0500

    Cambio variables settings

--- historia_clinica/settings.py
+++ historia_clinica/settings.py
@@ -112,7 +112,7 @@ if DATABASE_URL:  #manuel
 else:     #manuel
     DATABASES = {
             "default": {
-                "ENGINE": "django.db.backends.mysql",
+                "ENGINE": "django.db.backends.postgresql",
                 "NAME": config("DB_NAME"),
                 "USER": config("DB_USER"),
                 "PASSWORD": config("DB_PASSWORD"),
